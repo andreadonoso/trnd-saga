0ca91501ed112c1065b8530867e85e68f8c5e748
Andrea Donoso <donosoandrea@icloud.com>
2024-02-05 10:24:21 -0500

    create react app

--- backend/db.js
+++
@@ -1,6 +0,0 @@
-// module.exports = { 
-//     // Establish a connection to the database
-//     connectToDb: () => {}
-//     // Return the connection to the database 
-//     getDb: () => {}
-// }
