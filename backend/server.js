ba78ca6a5f13b04ab72ac065bbd0ed8a62180f08
Andrea Donoso <donosoandrea@icloud.com>
2024-02-04 22:23:19 -0500

    POST operation works

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app!!!'})
+    res.json({mssg: 'Welcome to the app!'})
 });
 
 // Listen for requests
