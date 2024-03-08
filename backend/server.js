7cad2a81e69a63a27ba2e49c41b48a1300b9902d
Andrea Donoso <donosoandrea@icloud.com>
2024-03-08 18:22:46 -0500

    heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,19 @@ app.use((req, res, next) => {
 	next();
 });
 
+// Heroku deployment
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+	app.get("*", (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname, "../", "frontend", "build", "index.html")
+		)
+	);
+} else {
+	app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 // Connect to MongoDB
 mongoose
 	.connect(URI)
