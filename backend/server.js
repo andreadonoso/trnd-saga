8665bb7a574fd628e5900aba75804577d98bc6b8
Andrea Donoso <donosoandrea@icloud.com>
2024-03-09 11:37:55 -0500

    fixed dev issues

--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,12 @@ app.use((req, res, next) => {
 
 // Heroku deployment
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-	app.get("*", (req, res) =>
+	app.use(express.static(path.join(__dirname, "../frontend", "build")));
+	app.get("*", (req, res) => {
 		res.sendFile(
-			path.resolve(__dirname, "../", "frontend", "build", "index.html")
-		)
-	);
+			path.join(__dirname, "../frontend", "build", "index.html")
+		);
+	});
 } else {
 	app.get("/", (req, res) => res.send("Please set to production"));
 }
